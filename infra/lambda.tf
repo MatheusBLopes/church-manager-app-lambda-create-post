505ad0cdf5f2a18aa390bfdd5a344f03e440411e
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-16 11:29:39 -0300

    [FEAT] - Update initial config

---
+++ infra/lambda.tf
@@ -0,0 +1,36 @@
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "../app/lambda_function.py"
+  output_path = "../app/lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.zip.output_path)
+
+  function_name = var.project_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 10
+  # publish       = true
+}
+
+resource "aws_lambda_alias" "alias_dev" {
+  name             = "dev"
+  description      = "dev"
+  function_name    = aws_lambda_function.lambda.arn
+  function_version = "$LATEST"
+}
+
+resource "aws_lambda_alias" "alias_prod" {
+  name             = "prod"
+  description      = "prod"
+  function_name    = aws_lambda_function.lambda.arn
+  function_version = "$LATEST"
+}
+
+
+resource "aws_cloudwatch_log_group" "convert_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+}
---
+++ publish.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# variables
+#
+
+# AWS variables
+AWS_REGION=sa-east-1
+
+# the directory containing the script file
+dir="$(cd "$(dirname "$0")"; pwd)"
+cd "$dir"
+
+[[ $1 != 'prod' && $1 != 'dev' ]] && { echo 'usage: publish.sh <prod | dev>'; exit 1; } ;
+
+# root account id
+ACCOUNT_ID=$(aws sts get-caller-identity \
+    --query Account \
+    --output text)
+
+echo aws lambda delete-alias $1
+aws lambda delete-alias \
+    --function-name $PROJECT_NAME \
+    --name $1 \
+    --region $AWS_REGION \
+    2>/dev/null
+
+echo zip lambda package
+cd app/
+rm --force lambda.zip
+zip lambda.zip ./*
+
+echo aws lambda update-function-code $PROJECT_NAME
+aws lambda update-function-code \
+    --function-name $PROJECT_NAME \
+    --zip-file fileb://hello.zip \
+    --region $AWS_REGION
+
+echo aws lambda update-function-code $PROJECT_NAME
+VERSION=$(aws lambda publish-version \
+    --function-name $PROJECT_NAME \
+    --description $1 \
+    --region $AWS_REGION \
+    --query Version \
+    --output text)
+echo published version: $VERSION
+
+echo aws lambda create-alias $1
+aws lambda create-alias \
+    --function-name $PROJECT_NAME \
+    --name $1 \
+    --function-version $VERSION \
+    --region $AWS_REGION
