be4f9e4e061d20c9f01f10a2a653f35ecd072188
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-16 23:36:56 -0300

    [FEAT] - Insert initial validation

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,2 +1,6 @@
+from src.schemas.input_event_schema import InputTest
+from aws_lambda_powertools.utilities.parser import parse
+
 def lambda_handler(event, context):
-    return { "statusCode": "200", "message": "É nóis"}
+    parsed_payload = parse(event=event, model=InputTest)
+    return { "statusCode": "200", "message": "É nóis", "payload": parsed_payload}
