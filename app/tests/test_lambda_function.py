182eec3c77c628d70ed5f56124bd138404ebd3f9
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-17 16:28:34 -0300

    [FEAT] - Update code

--- app/tests/test_lambda_function.py
+++ app/tests/test_lambda_function.py
@@ -2,4 +2,4 @@ from app.lambda_function import lambda_handler
 
 
 def test_lambda():
-    lambda_handler({"mesage": "test"}, {"test": "test"})
+    lambda_handler({"message": "test"}, {"test": "test"})
