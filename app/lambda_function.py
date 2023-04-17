81a68d52a7e5fa7c82606cdd92e1ebe7d66ed76c
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-17 15:25:03 -0300

    [FEAT] - Update code import

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -3,4 +3,4 @@ from app.src.schemas.input_event_schema import InputTest
 
 def lambda_handler(event, context):
     parsed_payload = InputTest(event)
-    return { "statusCode": "200", "message": "É nóis", "payload": parsed_payload}
+    return { "statusCode": "200", "message": "É nóis", "payload": parsed_payload.message}
--- app/src/schemas/input_event_schema.py
+++ app/src/schemas/input_event_schema.py
@@ -3,6 +3,3 @@ class InputTest():
 
     def __init__(self, message):
         self.message = message
-
-    def __str__(self):
-        return self.message
