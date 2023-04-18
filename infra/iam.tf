184685627691762623d05390ead1cef1f69cd39c
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-18 20:22:23 -0300

    [FEAT] - Attach new policy to the lambda role

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,3 +17,11 @@ resource "aws_iam_role" "lambda_role" {
   name               = "${var.project_name}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
+
+
+# Attach the AWSLambdaBasicExecutionRole policy to the Lambda role
+resource "aws_iam_policy_attachment" "lambda" {
+  name       = "lambda-policy-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  roles      = [aws_iam_role.lambda.name]
+}
