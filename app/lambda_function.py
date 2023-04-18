51a02e6fd9e3221be6a50f3d717dc0279b436411
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-18 19:58:45 -0300

    [FEAT] - Create database connection

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,6 +1,8 @@
 from app.src.schemas.input_event_schema import InputTest
 from aws_lambda_powertools.utilities.parser import parse
+from app.src.ports.database_repository import Database
 
 def lambda_handler(event, context):
     parsed_payload = parse(event=event, model=InputTest)
+    db = Database()
     return { "statusCode": "200", "message": "É nóis", "payload": parsed_payload.message}
---
+++ app/src/ports/database_repository.py
@@ -0,0 +1,70 @@
+import pymysql
+from aws_lambda_powertools.utilities import parameters
+
+class DatabaseConnectionSingleton:
+    __connection_instance = None
+
+    def __init__(self):
+        if DatabaseConnectionSingleton.__connection_instance is None:
+            try:
+                connection = pymysql.connect(
+                    host="terraform-20230418224750246200000001.cnjstxqpfga2.sa-east-1.rds.amazonaws.com",
+                    user="dbuser",
+                    password="debpassword",
+                    database="LifeManagerDb",
+                    port=5432,
+                    cursorclass=pymysql.cursors.DictCursor
+                )
+            except Exception as error:
+                raise error
+
+            DatabaseConnectionSingleton.__connection_instance = connection
+        else:
+            raise Exception("You can't instanciate another database connection")
+    
+    @staticmethod
+    def get_instance():
+        if DatabaseConnectionSingleton.__connection_instance is None:
+            DatabaseConnectionSingleton()
+        
+        return DatabaseConnectionSingleton.__connection_instance
+
+class Database():
+    def __init__(self):
+        self.connection = DatabaseConnectionSingleton.get_instance()
+        self.cursor = self.connection.cursor()
+    
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        return result
+
+    def save(self, insert_stmt: str, data: dict):
+        try:
+            self.cursor.execute(insert_stmt, data)
+            result = self.cursor.lastrowid
+            self.connection.commit()
+        
+        except Exception as error:
+            raise error
+
+        return result
+
+    def update(self, update_stmt: str, data: dict):
+        try:
+            result = self.cursor.execute(update_stmt, data)
+        
+        except Exception as error:
+            raise error
+        
+        return result
+
+    def delete(self, delete_stmt, data):
+        try:
+            self.cursor.execute(delete_stmt, data)
+            self.connection.commit()
+        except Exception as error:
+            raise error
+        
+        return True
