71dc0263c120430234e4586816aa08cac1b17b03
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-18 20:56:11 -0300

    [TEST] - Database connection test

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,8 +1,16 @@
 from app.src.schemas.input_event_schema import InputTest
 from aws_lambda_powertools.utilities.parser import parse
-from app.src.ports.database_repository import Database
+import pymysql
 
 def lambda_handler(event, context):
     parsed_payload = parse(event=event, model=InputTest)
-    db = Database()
+    connection = pymysql.connect(
+                    host="terraform-20230418224750246200000001.cnjstxqpfga2.sa-east-1.rds.amazonaws.com",
+                    user="dbuser",
+                    password="dbpassword",
+                    database="LifeManagerDb",
+                    port=5432,
+                    cursorclass=pymysql.cursors.DictCursor
+                )
+    connection.close()
     return { "statusCode": "200", "message": "É nóis", "payload": parsed_payload.message}
