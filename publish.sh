f9430bb944c0b66b0b7216233b4696c9e8f92d80
MatheusBLopes <matheusblopes10@gmail.com>
2023-04-30 22:56:55 -0300

    [FEAT] - Update publish

--- publish.sh
+++ publish.sh
@@ -41,7 +41,7 @@ aws lambda update-function-code \
     --region $AWS_REGION
 
 echo installing libfreetype6-dev
-sudo apt-get install libfreetype6-dev
+sudo apt-get install libfreetype6-dev libxft-dev  libjpeg62  libjpeg-devel
 
 
 echo zip layer package
